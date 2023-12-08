90c43d26359904bc3405e9e00d08915fd6dbc0a1
eunsung <fast129@naver.com>
2023-12-08 10:36:10 +0900

    숫자 중복없는 정답 만들기 & 스트라이크와 볼 갯수 확인

--- BaseballGame/main.swift
+++ BaseballGame/main.swift
@@ -7,5 +7,38 @@
 
 import Foundation
 
-print("Hello, World!")
+var inputNumbers = [Int]()
+var answerNumbers = [Int]()
+
+class BaseballGame {
+    func generateRandomNumber() -> [Int] {
+        var threeDigitNumber = [Int]()
+        while threeDigitNumber.count < 3 {
+            let randomNumber: Int = Int.random(in: 1...9)
+            if threeDigitNumber.contains(randomNumber) {
+                continue
+            } else {
+                threeDigitNumber.append(randomNumber)
+            }
+        }
+        return threeDigitNumber
+    }
+    
+    func strikeOrBall(inputNumbers: Int, answerNumbers: Int) -> Int {
+        var strikeCount = 0
+        var ballCount = 0
+        let input = String(inputNumbers).map { Int(String($0))! }
+        let answer = String(answerNumbers).map { Int(String($0))! }
+        for i in 0...2 {
+            guard input.contains(answer[i]) else { continue }
+            if i == input.firstIndex(of: answer[i]) {
+                strikeCount += 1
+            } else {
+                ballCount += 1
+            }
+        }
+        print("\(strikeCount) 스트라이크, \(ballCount) 볼")
+        return strikeCount
+    }
+}
 
